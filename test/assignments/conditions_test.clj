0e371e1f1de1c63e2b28ed38e8ebc909f43c85e5
sharmabhawna <bhawna.shar26@gmail.com>
2020-01-31 19:25:09 +0530

    Adds test for five-point-someone

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -31,3 +31,14 @@
            (is (= [2 2 4 5] (duplicate-first [2 4 5]))))
   (testing "empty collection"
            (is (nil? (duplicate-first [])))))
+
+(deftest five-point-someone-test
+  (testing "first argument is 5"
+           (is (= :satan-bhagat (five-point-someone 5 3))))
+  (testing "second argument is 5"
+           (is (= :chetan-bhagat (five-point-someone 3 5))))
+  (testing "first argument is greater than second argument"
+           (is (= :greece (five-point-someone 6 3))))
+  (testing "neither any argument is 5 nor first argument is greater than second"
+           (is (= :universe (five-point-someone 3 6))))
+  )
