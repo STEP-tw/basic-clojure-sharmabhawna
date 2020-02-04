60cd958ae8f67fb1f8c577115a3803330b3d8d26
sharmabhawna <bhawna.shar26@gmail.com>
2020-02-04 21:18:17 +0530

    Adds lists functions into core

--- src/assignments/core.clj
+++ src/assignments/core.clj
@@ -60,7 +60,6 @@
         (c/zero-separated-palindrome [:a])
         (c/zero-separated-palindrome [0]))))
 
-; list fns are to be filled by you
 (defn run-lists-fns
   []
   (do (println "List functions")
@@ -69,8 +68,89 @@
         (l/map' identity [1 2 3])
         (l/map' inc [1 2 3])
         (l/map' + [1 2 3] [4 5 6])
-        ;; fill the rest accordingly
-        )))
+        ;;
+        (l/filter' odd? [1 2 3 4 5])
+        (l/filter' even? [1 2 3 4 5])
+        (l/filter' (partial > 2) [1 2 3 4 5])
+        ;;
+        (l/reduce' + [1 2 3 4])
+        (l/reduce' + 10 [1 2 3 4])
+        ;;
+        (l/count' [1 2 3])
+        (l/count' {:one 1 :two 2})
+        (l/count' [1 \a "string" [1 2] {:foo :bar}])
+        ;;
+        (l/reverse' [1 2 3])
+        (l/reverse' {:one 1 :two 2})
+        (l/reverse' [1 \a "string" [1 2] {:foo :bar}])
+        ;;
+        (l/every?' odd? [1 3 5])
+        (l/every?' zero? [0 0])
+        (l/every?' even? [1 2 4])
+        ;;
+        (l/some?' odd? [1 2 4])
+        (l/some?' zero? [0 0])
+        (l/some?' even? [1 3 5])
+        ;;
+        (l/ascending? [1 2 3])
+        (l/ascending? [1 4 3])
+        ;;
+        (l/distinct' [1 2 3])
+        (l/distinct' [1 2 3 2 3 4])
+        ;;
+        (l/dedupe' [1 2 3])
+        (l/dedupe' [1 1 2 3 3])
+        ;;
+        (l/sum-of-adjacent-digits [1 2 3])
+        (l/sum-of-adjacent-digits [0 1 3 0])
+        ;;
+        (l/max-three-digit-sequence [1 3])
+        (l/max-three-digit-sequence [1 2 -1 2 0])
+        ;;
+        (l/transpose [[1 2 3] [4 5 6]])
+        ;;
+        (l/difference [1 2 3] [4 5 6])
+        ;;
+        (l/union [1 2 3] [4 5 6])
+        (l/union [1 2 3] [3 4 5])
+        (l/union [1 2 3 1] [4 5 6 3 2 4])
+        ;;
+        (l/points-around-origin)
+        ;;
+        (l/cross-product [1 2 3] [4 3 5]) ;
+        ;;
+        (l/double-up [1 2 3])
+        (l/double-up ["I" "love" "clojure"])
+        ;;
+        (l/third-or-fifth [1 2 3 "I" 4 "love" "clojure"])
+        ;;
+        (l/sqr-of-the-first [1 2 3])
+        ;;
+        (l/russian-dolls [1 4 3] 1)
+        (l/russian-dolls ["I" "hate" "you"] 3)
+        ;;
+        (l/split-comb [1 2 3 4])
+        (l/split-comb [1 2 3 4 5])
+        ;;
+        (l/muted-thirds [1 2 8 4 15 2 7])
+        ;;
+        (l/palindrome? [1 2 1])
+        (l/palindrome? [4 8 5])
+        ;;
+        (l/index-of [1 2 3] 1)
+        (l/index-of [1 2 3] "not there")
+        (l/index-of [1 2 3] 2)
+        ;;
+        (l/validate-sudoku-grid
+        [[4 3 5 2 6 9 7 8 1]
+         [6 8 2 5 7 1 4 9 3]
+         [1 9 7 8 3 4 5 6 2]
+         [8 2 6 1 9 5 3 4 7]
+         [3 7 4 6 8 2 9 1 5]
+         [9 5 1 7 4 3 6 2 8]
+         [5 1 9 3 2 6 8 7 4]
+         [2 4 8 9 5 7 1 3 6]
+         [7 6 3 4 1 8 2 5 9]]))))
 
 (defn -main [& args]
   (do (run-conditions-fns)
