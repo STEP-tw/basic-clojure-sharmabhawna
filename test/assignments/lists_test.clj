b25603bf2bba67b322507772be385254d64428f4
sharmabhawna <bhawna.shar26@gmail.com>
2020-02-05 20:09:31 +0530

    Implementing sum-of-adjacent-digits and adding tests for it

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -52,3 +52,11 @@
     (is (true? (ascending? [1 2 3]))))
   (testing "with incorrect sequence"
     (is (false? (ascending? [1 4 3])))))
+
+(deftest sum-of-adjacent-digits-test
+  (testing "with empty collection and single collection"
+    (are [x y] (= x y)
+               [] (sum-of-adjacent-digits [])
+               [] (sum-of-adjacent-digits [1])))
+  (testing "with collection containing more than single element"
+    (is (= [1 4 3] (sum-of-adjacent-digits [0 1 3 0])))))
